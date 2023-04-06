ca80e2c2cc702ccfd27556ef7fc2b13497f81984
dekusaaa <mengazhev2019@gmail.com>
2023-04-06 12:37:41 +0500

    commitLibrary

--- index.php
+++ index.php
@@ -7,6 +7,40 @@
     <title>Document</title>
 </head>
 <body>
-    test
+    
+
+
+
+
+
+
+
+
+
+
+<!-- connected lib -->
+<!-- bootstrap v.5 -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<!-- jquery 3.x -->
+<script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+
+
+<!-- aos -->
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+<script>
+  AOS.init();
+</script>
+
+<!-- connected lib -->
+<!-- connected your css/js -->
+<link rel="stylesheet" href="./css/main.css">
+<script src="./app/main.js"></script>
+
+
+<!-- connected your css/js -->
 </body>
 </html>
